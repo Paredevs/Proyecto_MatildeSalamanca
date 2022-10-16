86633f6d3145a92528200b66d51f85db90cc948b
Rodrigo Paredes <rodrigo.paredesp@userena.cl>
2022-10-16 01:21:29 -0300

    Version preeliminar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,11 +1,65 @@
-import './App.css';
+import "./App.css";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
+      <nav className="navbar">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1 "><img class="pequeña" src={logo} alt="" /><p>Colegio Matilde Salamanca</p></span>
+        </div>
+      </nav>
+      <div className="login">
+        <div className="login-box">
+          <div className="login-boxa">
+          <form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              RUT
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" class="form-text">
+              Tu correo no se compartira con nadie.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">
+              Contraseña
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              id="exampleCheck1"
+            />
+            <label class="form-check-label" for="exampleCheck1">
+              Mantener sesion iniciada
+            </label>
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Iniciar sesion
+          </button>
+          </form>
+          </div>
+        
+        </div>
+        
+      </div>
+      
       {/* <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        {/* <p>
+      {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> *
          <a
@@ -20,24 +74,8 @@ function App() {
       </header> */}
       <body>
         {/* <img src="../public/logo.png" className="App-logo" alt="logo" /> */}
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Correo electronico</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-            <div id="emailHelp" class="form-text">Tu correo no se compartira con nadie.</div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-            <input type="password" class="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-          <label class="form-check-label" for="exampleCheck1">Mantener sesion iniciada</label>
-        </div>
-            <button type="submit" class="btn btn-primary">Iniciar sesion</button>
-        </form>
+        
       </body>
-      
     </div>
   );
 }
