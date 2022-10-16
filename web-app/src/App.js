1e9e70ff7445e2658df6b999e221e18a5dfa74e6
Rodrigo Paredes <rodrigo.paredesp@userena.cl>
2022-10-15 23:07:28 -0300

    Ajustes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,19 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <p>Hola mundo</p>
       </header>
+      <body>
+        <p>Hola</p>
+      </body>
+      
     </div>
   );
 }
